515add637a89b1d64d35b54ea6298ac61e91d144
Gusram <idabgsram@gmail.com>
2021-06-12 00:33:30 +0800

    update main

--- lib/social_share.dart
+++ lib/social_share.dart
@@ -191,6 +191,29 @@ class SocialShare {
     return version;
   }
 
+  static Future<String> shareWhatsappStatus(String contentText,{String imagePath}) async {
+    
+    Map<String, dynamic> args;
+
+    if (imagePath != null) {
+        File file = File(imagePath);
+        Uint8List bytes = file.readAsBytesSync();
+        var imagedata = bytes.buffer.asUint8List();
+        final tempDir = await getTemporaryDirectory();
+        String imageName = 'stickerAsset.png';
+        final Uint8List imageAsList = imagedata;
+        final imageDataPath = '${tempDir.path}/$imageName';
+        file = await File(imageDataPath).create();
+        file.writeAsBytesSync(imageAsList);
+        args = <String, dynamic>{"image": imageName, "content": contentText};
+      } else {
+        args = <String, dynamic>{"image": imagePath, "content": contentText};
+      }
+      
+    final String version = await _channel.invokeMethod('shareWhatsapp', args);
+    return version;
+  }
+
   static Future<Map> checkInstalledAppsForShare() async {
     final Map apps = await _channel.invokeMethod('checkInstalledApps');
     return apps;
