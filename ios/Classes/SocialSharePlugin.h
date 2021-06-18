46060612130a9ad454867901038fd93f27935821
Gusram <idabgsram@gmail.com>
2021-06-18 15:28:32 +0800

    test 112

--- ios/Classes/SocialSharePlugin.h
+++ ios/Classes/SocialSharePlugin.h
@@ -1,4 +1,16 @@
 #import <Flutter/Flutter.h>
 
 @interface SocialSharePlugin : NSObject<FlutterPlugin>
+
+// Whatsapp UTI
+typedef enum {
+	WhatsAppImageType = 0,
+	WhatsAppAudioType,
+	WhatsAppVideoType
+} WhatsAppType;
+
++ (id)getInstance;
+- (void)sendText:(NSString*)message;
+- (void)sendFile:(NSData *)data UTI:(WhatsAppType)type inView:(UIView *)view;
+
 @end
--- ios/Classes/SocialSharePlugin.m
+++ ios/Classes/SocialSharePlugin.m
@@ -6,13 +6,111 @@
 #import "SocialSharePlugin.h"
 #include <objc/runtime.h>
 
+// Whatsapp URLs
+NSString *const whatsAppUrl = @"whatsapp://app";
+NSString *const whatsAppSendTextUrl = @"whatsapp://send?text=";
+
+// Whatsapp UTI
+NSString *UTIWithWhatsAppType(WhatsAppType type) {
+	NSArray *arr = @[
+					 @"net.whatsapp.image", //image
+					 @"net.whatsapp.audio", //audio
+					 @"net.whatsapp.movie"  //movie
+					 ];
+	return (NSString *)[arr objectAtIndex:type];
+}
+
+NSString *typeWithWhatsAppType(WhatsAppType type) {
+	NSArray *arr = @[
+					 @"whatsAppTmp.wai", //image
+					 @"whatsAppTmp.waa", //audio
+					 @"whatsAppTmp.wam"  //movie
+					 ];
+	return (NSString *)[arr objectAtIndex:type];
+}
+
+// Instace
+__strong static SocialSharePlugin* instanceOf = nil;
+
 @interface SocialSharePlugin()<UIDocumentInteractionControllerDelegate>{
-	UIDocumentInteractionController *docInterationController;
+	UIDocumentInteractionController *_docControll;
 }
 
 @end
 
 @implementation SocialSharePlugin
+
++ (SocialSharePlugin*)getInstance
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		instanceOf = [[SocialSharePlugin alloc] init];
+	});
+	return instanceOf;
+}
+
+- (NSURL *)createTempFile:(NSData *)data type:(NSString *)type
+{
+	NSError *error = nil;
+	NSURL *tempFile = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory
+																inDomain:NSUserDomainMask
+													appropriateForURL:nil
+																  create:NO
+																error:&error];
+	
+	if (tempFile)
+	{
+		tempFile = [tempFile URLByAppendingPathComponent:type];
+	} else {
+		[self alertErro:[NSString stringWithFormat:@"Error getting document directory: %@", error]];
+	}
+	
+	if (![data writeToURL:tempFile options:NSDataWritingAtomic error:&error]){
+		[self alertErro:[NSString stringWithFormat:@"Error writing File: %@", error]];
+	}
+	
+	return tempFile;
+}
+
+- (void)alertWithTitle:(NSString *)title message:(NSString *)message
+{
+	UIViewController *vc = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+																   message:message
+															preferredStyle:UIAlertControllerStyleAlert];
+	
+	[alert addAction:[UIAlertAction actionWithTitle:@"OK"
+										   style:UIAlertActionStyleCancel
+										 handler:^(UIAlertAction *action) {
+											 
+		 [vc dismissViewControllerAnimated:YES completion:^{}];
+	 }]];
+	
+	[vc presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)alertErro:(NSString *)message
+{
+	[self alertWithTitle:@"Error" message:message];
+}
+
+- (void)sendFile:(NSData *)data UTI:(WhatsAppType)type inView:(UIView *)view
+{
+	if ( [self isWhatsAppInstalled] )
+	{
+		NSURL *tempFile	= [self createTempFile:data type:typeWithWhatsAppType(type)];
+		_docControll = [UIDocumentInteractionController interactionControllerWithURL:tempFile];
+		_docControll.UTI = UTIWithWhatsAppType(type);
+		_docControll.delegate = self;
+		
+		[_docControll presentOpenInMenuFromRect:CGRectZero
+										 inView:view
+									   animated:YES];
+	} else {
+		[self alertWhatsappNotInstalled];
+	}
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel methodChannelWithName:@"social_share" binaryMessenger:[registrar messenger]];
   SocialSharePlugin* instance = [[SocialSharePlugin alloc] init];
@@ -334,21 +432,27 @@
                         NSString    * savePath  = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/whatsAppTmp.wai"];
 
                         [UIImageJPEGRepresentation(iconImage, 1.0) writeToFile:savePath atomically:YES];
-
-
                         NSFileManager *fileManager = [NSFileManager defaultManager];
                         BOOL isFileExist = [fileManager fileExistsAtPath: savePath];
                         UIImage *imgShare;
                         if (isFileExist) {
                             imgShare = [[UIImage alloc] initWithContentsOfFile:savePath];
                         }
-                        NSArray *objectsToShare = @[imgShare];
-                        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
-                        UIViewController *controller =[UIApplication sharedApplication].keyWindow.rootViewController;
-                        [controller presentViewController:activityVC animated:YES completion:nil];
-                        result([NSNumber numberWithBool:YES]);
+                        [self sendImage:imgShare inView:self.view];
 
-                        // docInterationController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
+                        // NSFileManager *fileManager = [NSFileManager defaultManager];
+                        // BOOL isFileExist = [fileManager fileExistsAtPath: savePath];
+                        // UIImage *imgShare;
+                        // if (isFileExist) {
+                        //     imgShare = [[UIImage alloc] initWithContentsOfFile:savePath];
+                        // }
+                        // NSArray *objectsToShare = @[imgShare];
+                        // UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
+                        // UIViewController *controller =[UIApplication sharedApplication].keyWindow.rootViewController;
+                        // [controller presentViewController:activityVC animated:YES completion:nil];
+                        // result([NSNumber numberWithBool:YES]);
+
+                        // UIDocumentInteractionController *docInterationController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
                         // docInterationController.UTI = @"net.whatsapp.image";
                         // docInterationController.delegate = self;
 
