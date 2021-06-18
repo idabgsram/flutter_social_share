036d8b6cb0d467d6acf6a9b20b65a051975a5089
Gusram <idabgsram@gmail.com>
2021-06-18 15:45:02 +0800

    test 113

--- ios/Classes/SocialSharePlugin.h
+++ ios/Classes/SocialSharePlugin.h
@@ -1,16 +1,4 @@
 #import <Flutter/Flutter.h>
 
 @interface SocialSharePlugin : NSObject<FlutterPlugin>
-
-// Whatsapp UTI
-typedef enum {
-	WhatsAppImageType = 0,
-	WhatsAppAudioType,
-	WhatsAppVideoType
-} WhatsAppType;
-
-+ (id)getInstance;
-- (void)sendText:(NSString*)message;
-- (void)sendFile:(NSData *)data UTI:(WhatsAppType)type inView:(UIView *)view;
-
 @end
